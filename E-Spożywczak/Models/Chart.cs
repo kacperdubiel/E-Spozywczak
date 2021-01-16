bf818635061692ec55f31bea1e7f95ab75955b13
majajurenczyk <majka.jurenczyk@gmail.com>
2021-01-16 22:14:38 +0100

    Updated Models

---
+++ E-Spożywczak/Models/Chart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Spożywczak.Models
+{
+    public class Chart
+    {
+        public int ChartId;
+        ICollection<ProductInChart> ProductsInChartList;
+    }
+}
---
+++ E-Spożywczak/Models/Delivery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Spożywczak.Models
+{
+    public class Delivery
+    {
+        public int DeliveryId;
+        public string DeliveryTypeName;
+        public double DeliveryPrice;
+        public ICollection<Order> DeliveryOrdersList;
+    }
+}
