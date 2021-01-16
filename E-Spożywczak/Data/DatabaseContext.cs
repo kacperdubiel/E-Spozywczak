457a8997ffad6918d40b55b1ea8b3fee8ba830ba
kacperdubiel <kacper.dubiel13@gmail.com>
2021-01-16 22:58:16 +0100

    Bug fix

--- E-Spożywczak/Data/DatabaseContext.cs
+++ E-Spożywczak/Data/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using E_Spożywczak.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,7 @@ namespace E_Spożywczak.Data
         public DbSet<Cart> Cart { get; set; }
         public DbSet<Delivery> Delivery { get; set; }
         public DbSet<Order> Order { get; set; }
-        public DbSet<OrderHistory> OrderHistory { get; set; }
+        public DbSet<OrdersHistory> OrderHistory { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<ProductCategory> ProductCategory { get; set; }
         public DbSet<ProductInCart> ProductInCart { get; set; }
