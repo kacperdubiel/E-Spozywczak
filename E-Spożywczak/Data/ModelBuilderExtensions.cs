9d14cd17998528212b58ba4a5cfc1e55e007e1d5
kacperdubiel <kacper.dubiel13@gmail.com>
2021-01-16 22:44:48 +0100

    Updated configuration, added DbContext with empty seed

---
+++ E-Spożywczak/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Spożywczak.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+           // TODO: Add seed
+        }
+    }
+}
