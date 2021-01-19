4d0a679788687283b186b05720fbd7ffd26dbb91
kacperdubiel <kacper.dubiel13@gmail.com>
2021-01-19 17:39:59 +0100

    Added basic Cart page

---
+++ E-Spożywczak/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using E_Spożywczak.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Spożywczak.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public CartController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Models.ProductInCart> productsInCart = await _context.ProductInCart.ToListAsync();
+
+            return View("Index", productsInCart);
+        }
+    }
+}
